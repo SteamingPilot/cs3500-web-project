85f4c712b79affece22bc599b346e3cfe58c7438
SteamingPilot <md.palok@gmail.com>
2022-12-10 10:12:28 +0600

    db

--- dbschema/playnchat.sql
+++ dbschema/playnchat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2022 at 01:47 AM
+-- Generation Time: Dec 10, 2022 at 05:11 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -24,6 +24,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `game`
+--
+
+CREATE TABLE `game` (
+  `gameId` int(11) NOT NULL,
+  `p1` int(11) DEFAULT NULL,
+  `p2` int(11) DEFAULT NULL,
+  `turn` int(11) DEFAULT NULL,
+  `board` varchar(50) DEFAULT NULL,
+  `wins` int(11) DEFAULT NULL,
+  `losses` int(11) DEFAULT NULL,
+  `draws` int(11) DEFAULT NULL,
+  `gameType` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invite`
+--
+
+CREATE TABLE `invite` (
+  `inviteId` int(11) NOT NULL,
+  `invitee` int(11) NOT NULL,
+  `inviter` int(11) NOT NULL,
+  `gameId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -32,20 +63,40 @@ CREATE TABLE `user` (
   `FirstName` varchar(30) NOT NULL,
   `LastName` varchar(30) NOT NULL,
   `Email` varchar(30) NOT NULL,
-  `Password` longtext NOT NULL
+  `Password` longtext NOT NULL,
+  `isPlaying` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`Id`, `FirstName`, `LastName`, `Email`, `Password`) VALUES
-(1, 'Paul', 'Palok', 'md.palok@gmail.com', '$2y$10$2R12ps3pviE1A/V9EZuT/OJz3V1I2pO030tBkF8aswJ3yv1BNrpqa'),
-(2, 'Tarin', 'Nurany', 'tarin@gmail.com', '$2y$10$axlJrOF606ybXpRuKrkojuRWhtEdhPUIpfQYP1MG9zPOpc3nMTQmu');
+INSERT INTO `user` (`Id`, `FirstName`, `LastName`, `Email`, `Password`, `isPlaying`) VALUES
+(1, 'Paul', 'Palok', 'md.palok@gmail.com', '$2y$10$2R12ps3pviE1A/V9EZuT/OJz3V1I2pO030tBkF8aswJ3yv1BNrpqa', 0),
+(2, 'Tarin', 'Nurany', 'tarin@gmail.com', '$2y$10$axlJrOF606ybXpRuKrkojuRWhtEdhPUIpfQYP1MG9zPOpc3nMTQmu', 0),
+(3, 'Abir', 'Palok', 'abir.palok@hotmail.com', '$2y$10$FavdV/0BQh35tA6Ws9PoD.R3dkYjoHPRAlArIjGbQ91Qjf3VveXRG', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `game`
+--
+ALTER TABLE `game`
+  ADD PRIMARY KEY (`gameId`),
+  ADD KEY `p1` (`p1`),
+  ADD KEY `p2` (`p2`),
+  ADD KEY `turn` (`turn`);
+
+--
+-- Indexes for table `invite`
+--
+ALTER TABLE `invite`
+  ADD PRIMARY KEY (`inviteId`),
+  ADD KEY `invitee` (`invitee`),
+  ADD KEY `inviter` (`inviter`),
+  ADD KEY `gameId` (`gameId`);
 
 --
 -- Indexes for table `user`
@@ -59,10 +110,36 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `invite`
+--
+ALTER TABLE `invite`
+  MODIFY `inviteId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `game`
+--
+ALTER TABLE `game`
+  ADD CONSTRAINT `game_ibfk_1` FOREIGN KEY (`p1`) REFERENCES `user` (`Id`),
+  ADD CONSTRAINT `game_ibfk_2` FOREIGN KEY (`p2`) REFERENCES `user` (`Id`),
+  ADD CONSTRAINT `game_ibfk_3` FOREIGN KEY (`turn`) REFERENCES `user` (`Id`);
+
+--
+-- Constraints for table `invite`
+--
+ALTER TABLE `invite`
+  ADD CONSTRAINT `gameId` FOREIGN KEY (`gameId`) REFERENCES `game` (`gameId`),
+  ADD CONSTRAINT `invitee` FOREIGN KEY (`invitee`) REFERENCES `user` (`Id`),
+  ADD CONSTRAINT `inviter` FOREIGN KEY (`inviter`) REFERENCES `user` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
